ccf80682e13ac98ffdcd0064888c36bd1155a4a0
Fawn <rubiefawn@gmail.com>
2025-02-25 18:27:10 -0700

    Add inverted planeswalker symbol icon/holos stamp
    I use these to visually mark my proxies in a way that is not visually
    distracting, but that very clearly indicates a non-genuine card

--- js/frames/packTheList.js
+++ js/frames/packTheList.js
@@ -1,7 +1,9 @@
 //defines available frames
 availableFrames = [
 	{name:'Post-M15', src:'/img/frames/m15/theList/regular.svg'},
-	{name:'Pre-M15', src:'/img/frames/m15/theList/old.svg'}
+	{name:'Pre-M15', src:'/img/frames/m15/theList/old.svg'},
+	{name:'Proxy Post-M15', src:'/img/frames/m15/theList/proxy-m15.svg'},
+	{name:'Proxy Pre-M15', src:'/img/frames/m15/theList/proxy-7ed.svg'},
 ];
 //disables/enables the "Load Frame Version" button
 document.querySelector('#loadFrameVersion').disabled = true;
