6ba62bdea8b3753224b69ba8986642ab05210bad
AModSoul <109749060+AModSoul@users.noreply.github.com>
2025-05-03 01:11:37 -0400

    Fix Indentation

--- js/frames/versionClass.js
+++ js/frames/versionClass.js
@@ -37,16 +37,8 @@ if (!loadedVersions.includes('/js/frames/versionClass.js')) {
 	// placement for header
 function getCardClass() {
 	switch (card.version) {
-		case 'class':
-			return {
-				x: 0.5014,
-				width: 0.422
-			};
-		default:
-			return {
-				x: 0.5014,
-				width: 0.422
-			};
+		case 'class': return {x: 0.5014, width: 0.422};
+		default: return { x: 0.5014, width: 0.422};
 	}
 } 
 	// use correct header image
